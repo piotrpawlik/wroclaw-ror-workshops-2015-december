faf7b92145bc1c813f4c06ef5ba5fe134b65e65e
Emanuel <siawmensah@gmail.com>
2015-11-22 20:34:12 +0100

    Add routes for users/subjects

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+	def password=(password)
+		@password = password
+	end
 end

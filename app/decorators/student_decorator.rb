06c64fdc530bc2e4ad5a644d458a3f859a6e11f3
Emanuel <siawmensah@gmail.com>
2015-11-30 23:11:16 +0100

    Clean up student_decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,13 +4,7 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
-  	if subject_item == 0
-  		sprintf('%.2f', subject_item)
-  	else
-  		average_notes = if !subject_item.subject_item_notes.size.zero?
-  			subject_item.subject_item_notes.map(&:value).sum / subject_item.subject_item_notes.size.to_f
-  		end
-  		sprintf('%.2f', average_notes.to_f)
-  	end
+    average = subject_item_notes.for_subject_item(subject_item).average(:value)
+    average.nil? ? "0.00" : ('%.2f' % average)
   end
 end

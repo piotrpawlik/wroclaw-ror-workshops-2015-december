5065ad4649cc179b5a86f51e9accf764f9041ec7
Emanuel <siawmensah@gmail.com>
2015-11-30 20:19:44 +0100

    Add reports namespace as report

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
   #edit later to only allow actions specified in specs.
   resources :teachers, :visitors, :report_subjects, :teacher_subjects
 
+  namespace :reports, as: :report do
+  	get :subjects
+  end
+
   root 'visitors#index'
 end

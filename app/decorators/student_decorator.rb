f82f2b0bee2ca19d1bce8350f1bcb350f714a67a
Emanuel <siawmensah@gmail.com>
2015-11-30 01:56:34 +0100

    Define avg_notes method

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,10 +4,13 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
-  	if subject_item = 0
+  	if subject_item == 0
   		sprintf('%.2f', subject_item)
   	else
-
+  		average_notes = if !subject_item.subject_item_notes.size.zero?
+  			subject_item.subject_item_notes.map(&:value).sum / subject_item.subject_item_notes.size.to_f
+  		end
+  		sprintf('%.2f', average_notes.to_f)
   	end
   end
 end
